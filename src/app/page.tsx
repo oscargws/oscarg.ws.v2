b499db8b476a683cac02c54601ceb63dbeea282f
Oscar <40099755+oscargws@users.noreply.github.com>
2025-12-12 21:49:18 +1100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import WritingSection from "./components/WritingSection";
 
 export const metadata = {
   title: `Oscar Watson-Smith`,
-  description: `I've worked in the tech industry for over a decade wearing many hats. I've built products as an engineer, shifted between frontend and backend, to ultimately end up as people facing as I could get, implementing solutions for them.`,
+  description: `I've spent over a decade in tech—started as an engineer bouncing between frontend and backend, eventually finding my way to the customer-facing side as a sales engineer. Turns out I like the people part as much as the building part.`,
   keywords: [
     "Oscar",
     "Watson-Smith",
@@ -21,12 +21,15 @@ export default function Home() {
       <div className="max-w-xl gap-6 flex flex-col mt-8 sm:mt-24 px-4">
         <h1 className="text-4xl">{`👋 Hey, I'm Oscar`}</h1>
         <p className="text-lg text-zinc-600">
-          {`I've worked in the tech industry for over a decade wearing many hats. I've built products as an engineer, shifted between frontend and backend, to ultimately end up as people facing as I could get, implementing solutions for them.`}
+          {`I've spent over a decade in tech—started as an engineer bouncing between frontend and backend, eventually finding my way to the customer-facing side as a sales engineer. Turns out I like the people part as much as the building part.`}
         </p>
         <p className="text-lg text-zinc-600">
           {`Throughout it all I've had a genuine love of technology and what it can do. The novelty of being able to build anything, and share it with anyone, is yet to wear off on me.`}
         </p>
-         <p className="text-lg text-zinc-600">
+        <p className="text-lg text-zinc-600">
+          {`I'm currently working on `<a href="https://redbark.co">Redbark</a>{`, a platform for Aussie's to manage their finances and net worth with their own hands.`}
+        </p>
+        <p className="text-lg text-zinc-600">
           {`I also angel invest in early stage startups operating in the design, frontend and observability space. If you're buiding in this area, I'd love to chat.`}
         </p>
       </div>
